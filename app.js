288080c31a025e7ebc4eb46923cd5326a6104aca
vicky <srivicky2000@gmail.com>
2023-07-13 15:34:43 +0530

    user login in prod staging

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 require("dotenv").config();
 const express = require('express');
 const app = express();
