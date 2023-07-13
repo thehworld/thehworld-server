eaf7c9dfd78c2c45efd233f6805fe81775532727
vicky <srivicky2000@gmail.com>
2023-07-13 10:54:00 +0530

    user cart add and revome here

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsConfig = {
     credentials: true,
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"]
 };
-app.use(cors());
+app.use(cors(corsConfig));
 app.options('https://rococo-banoffee-61f602.netlify.app', cors());
 
 
@@ -111,6 +111,33 @@ const options = {
     cert: fs.readFileSync('./.cert/cert.pem')
 };
 // ?
+
+
+
+
+///
+
+// Add headers before the routes are defined
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://rococo-banoffee-61f602.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+///
 
 
 
