a8beb91d56b8f26d61b3038c1b02c219cea2eb30
vicky <srivicky2000@gmail.com>
2023-07-13 13:15:27 +0530

    user cart add and revome here

--- app.js
+++ app.js
@@ -22,6 +22,19 @@ const corsOptions = {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
 }
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.header('Origin'));
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
 app.use(cors(corsOptions));
 
 app.options('*', cors());
