de7f2421f2d1868cdfd96e3dbfe31b584214993e
Sri Vigneshwar DJ <srivicky2000@gmail.com>
2023-08-21 20:10:27 +0530

    order refund

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,6 +1,17 @@
 const pigcolor = require('pigcolor');
+const { v4: uuidv4 } = require('uuid')
+const jwt = require('jsonwebtoken');
+const jwt_decode = require('jwt-decode');
+var base64 = require('base-64');
+var CryptoJS = require("crypto-js");
+var SHA256 = require("crypto-js/sha256");
+const { Base64 } = require('js-base64');
+const axios = require('axios');
+
 const Order = require("../models/orders");
 const User = require("../models/users");
+
+
 
 exports.createOrder = (req, res) => {
     pigcolor.box("Create: New Order");
@@ -127,28 +138,107 @@ exports.changeOrderStatus = (req, res) => {
         }
         console.log(order);
         order.orderStatus = req.body.status.status;
-        order.save().then((newOrder, err) => {
 
-            if (err) {
-                return res.json({
-                    error: err
-                })
+        if (req.body.status.status === "REFUND INIT") {
+
+            // Generate PhonePe Payment Body
+            const PhonePePaymentBody = {
+                "merchantId": "THEHWORLDONLINE",
+                "merchantTransactionId": req.body.status.orderId,
+                "transactionId": req.body.status.orderId,
+                "providerReferenceId": req.body.status.paymentResponse.data.transactionId,
+                "amount": req.body.status.orderTotal * 100,
+                "merchantOrderId": req.body.status.orderId,
+                "message": "refund for cancelled order"
             }
-            if (!newOrder) {
+            console.log("Generated Payment Body - ", PhonePePaymentBody);
+            var encodedData = base64.encode(JSON.stringify(PhonePePaymentBody));
+            console.log("Generated Payment Body Base64 - ", encodedData);
+            var salt = "17cfc168-1045-43cb-88b1-ad26c042f233"
+            var x_verify_payload = encodedData + "/v3/credit/backToSource" + salt
+            console.log("x-verify Payload - ", x_verify_payload);
+            var x_verify = SHA256(x_verify_payload) + "###1";
+            console.log("x-verify  - ", x_verify);
+
+
+
+            let data = JSON.stringify({
+                "request": encodedData
+            });
+            let config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: 'https://api.phonepe.com/apis/hermes/pg/v1/refund',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-VERIFY': x_verify
+                },
+                data: data
+            };
+
+            axios.request(config).then((response) => {
+                console.log(response.data);
+                if (response.data.success) {
+                    order.orderStatus = "REFUNDED";
+                    order.save().then((newOrder, err) => {
+
+                        if (err) {
+                            return res.json({
+                                error: err
+                            })
+                        }
+                        if (!newOrder) {
+                            return res.json({
+                                error: "Order don't exist"
+                            })
+                        }
+
+                        return res.json({
+                            order: newOrder,
+                            paymentURlredirect: response.data.instrumentResponse,
+                            paymentObject: response.data,
+                            paymentStatus: true
+                        })
+
+                    }).catch((error) => {
+                        return res.json({
+                            error: error
+                        })
+                    });
+                }
+
+
+            }).catch((err) => {
+                console.log("Error - ", err);
+            });
+
+
+        } else {
+
+            order.save().then((newOrder, err) => {
+
+                if (err) {
+                    return res.json({
+                        error: err
+                    })
+                }
+                if (!newOrder) {
+                    return res.json({
+                        error: "Order don't exist"
+                    })
+                }
+
                 return res.json({
-                    error: "Order don't exist"
+                    order: newOrder
                 })
-            }
 
-            return res.json({
-                order: newOrder
-            })
+            }).catch((error) => {
+                return res.json({
+                    error: error
+                })
+            });
+        }
 
-        }).catch((error) => {
-            return res.json({
-                error: error
-            })
-        });
 
     }).catch((err) => {
         if (err) {
--- models/orders.js
+++ models/orders.js
@@ -30,7 +30,7 @@ const orderSchema = new mongoose.Schema({
 
     orderStatus: {
         type: String,
-        enum: ["NEW", "ACCEPTED", "DISPATCHED", "SHIPPED", "OUTFORDELIVERY", "DELIVERED"],
+        enum: ["NEW", "ACCEPTED", "DISPATCHED", "SHIPPED", "OUTFORDELIVERY", "DELIVERED", "RETURN INIT", "RETURN DONE", "REFUND INIT", "REFUNDED", "CANCELLED"],
         default: "NEW"
     },
 
