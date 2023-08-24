0581acdcdfc73fd3ce5f320e961ece4fe6dfbd69
Sri Vigneshwar DJ <srivicky2000@gmail.com>
2023-08-24 15:52:09 +0530

    order problem

--- controllers/issues.js
+++ controllers/issues.js
@@ -1,7 +1,25 @@
 const pigcolor = require('pigcolor');
-
+const Issue = require('../models/issues');
 
 exports.issueProblem = (req, res) => {
     pigcolor.box("Problem: Issue");
     console.log("Problem - ", req.body);
+    const newIssue = new Issue();
+    newIssue.order = req.body.order;
+    newIssue.status = "NEW";
+    newIssue.save()
+        .then((problm, err) => {
+            if (err) {
+                return res.json({
+                    error: err
+                })
+            }
+            return res.json({
+                problm: problm
+            })
+        }).catch((err) => {
+            return res.json({
+                error: err
+            })
+        });
 }
