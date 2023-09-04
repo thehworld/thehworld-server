24a1356e17db44cac3275a2012e67e18e5c0915c
Sri Vigneshwar DJ <srivicky2000@gmail.com>
2023-09-04 11:55:30 +0530

    offer control

--- controllers/offer.js
+++ controllers/offer.js
@@ -66,3 +66,61 @@ exports.getOfferCodeValue = (req, res) => {
         })
     });
 }
+
+
+exports.changeStatusOffer = (req, res) => {
+    pigcolor.box("Change: Offer Status");
+    console.log("Offer Body - ", req.body);
+    Offer.findOne({ _id: req.body.id })
+        .then((offer, err) => {
+            if (err) {
+                return res.json({
+                    error: err
+                })
+            }
+            console.log("Error - ", offer);
+            if (offer.status === "ACTIVE") {
+                offer.status = "INACTIVE"
+            } else {
+                offer.status = "ACTIVE"
+            }
+            offer.save().then((newOffer, err) => {
+                if (err) {
+                    return res.json({
+                        error: err
+                    })
+                }
+                return res.json({
+                    offer: newOffer
+                })
+            }).catch((err) => {
+                return res.json({
+                    error: err
+                })
+            });
+        }).catch((err) => {
+            return res.json({
+                error: err
+            })
+        });
+}
+
+exports.deleteOfferCode = (req, res) => {
+    pigcolor.box("DELETE: Offer");
+    console.log(req.body);
+    Offer.findOneAndDelete({ _id: req.body.id })
+        .then((offer, err) => {
+            if (err) {
+                return res.json({
+                    error: err
+                })
+            }
+            return res.json({
+                offer: offer
+            })
+        }).catch((err) => {
+            return res.json({
+                error: err
+            })
+        });
+}
